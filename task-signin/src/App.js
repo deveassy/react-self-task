fa27237023fe3ccb8fa36b0b22abfe00f315d3f2
deveassy <xjfmwld@gmail.com>
2020-04-09 23:31:10 +0900

    과제5-회원가입창 만들기 수정본
    useState에 대해 다시 공부하고 습득

--- task-signin/src/App.js
+++ task-signin/src/App.js
@@ -1,33 +1,72 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
-const [id, setId] = useState("");
-const [pasw, setPasw] = useState("");
-const [name, setName] = useState("");
-const [email, setEmail] = useState("");
+export default function App() {
 
-useEffect(() => {
-  console.log({
-    id,
-    pasw,
-    name,
-    email
+  const [state, setState] = useState({
+    id:'',
+    password:'',
+    password2:'',
+    name:'',
+    email:''
   });
-});
 
-export default function App(props) {
+  function handleChange(e) {
+    const updateState = {
+    ...state, //state의 모든 값들을 가지고 와서
+    [e.target.name] : e.target.value //모든 input의 name값들에 대한 value값을 표시
+    };
+    setState(updateState);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(state);
+  }
+
   return (
     <SignIn>
-      <input value={id} onChange={onChangeId} placeholder="id" />
-      <input value="pasw" type="password" placeholder="password" />
-      <input
-        value="confirmPasw"
-        type="password"
-        placeholder="confrim password"
-      />
-      <input value="name" placeholder="name" />
-      <input value="email" placeholder="e-mail" />
-      <button onClick={() => setSubmit(console.log(submit))}>SUBMIT</button>
+      <form
+        onSubmit={handleSubmit}
+      >
+        <input
+          type="text"
+          name="id"
+          value={state.id}
+          onChange={handleChange}
+          placeholder="id"
+        />
+        <input
+          type="password"
+          name="pwd"
+          value={state.pwd}
+          onChange={handleChange}
+          placeholder="password"
+        />
+        <input
+          type="password"
+          name="pwd2"
+          value={state.pwd2}
+          onChange={handleChange}
+          placeholder="confrim password"
+          onBlur={() => alert("비밀번호를 확인해주세요") } // input창을 떠나게 될 때 메세지가 뜬다
+        />
+        <input
+          type="text"
+          name="name"
+          value={state.name}
+          onChange={handleChange}
+          placeholder="name"
+        />
+        <input
+          type="email" // 이메일 형식이 맞지 않으면 경고창이 뜨게 되있음
+          name="email"
+          value={state.email}
+          onChange={handleChange}
+          placeholder="e-mail"
+        />
+        <button type="submit">SUBMIT</button>
+      </form>
     </SignIn>
   );
 }
